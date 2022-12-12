7a7b5b0b0e81873bd5abbbda174a6edf198005b3
JorgeRC <jorgercdeveloper@gmail.com>
2022-12-12 00:05:20 +0000

    Constraint layout

--- app/src/main/java/com/app/kotlinapp_03/MainActivity.kt
+++ app/src/main/java/com/app/kotlinapp_03/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        setContentView(R.layout.linear_layout)
+        setContentView(R.layout.constraint_layout)
     }
 }
